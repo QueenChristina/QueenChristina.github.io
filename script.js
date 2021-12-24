516c02177ae2a8f7e4fd277f3688f00393b3d162
QueenChristina <qinkristina@gmail.com>
2021-12-23 21:21:21 -0800

    Compress media files, make focus image overlay center, add captions to games

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function show(page) {
+    // Switches to the page indicated by pressing menu button
     // Assert page should be an id here
     let allPageIds = ['art', 'animation', 'games/tools'];
     
@@ -7,4 +8,15 @@ function show(page) {
     }
 
     document.getElementById(page).style.display = 'initial';
+    document.getElementById('content').scrollTop = 0;
+}
+
+// Show closeups of image
+function focusThis(img) {
+    document.getElementById('focusImg').src = img.src;
+    document.getElementById('overlayImg').style.display = 'initial';
+}
+
+function hideOverlay() {
+    document.getElementById('overlayImg').style.display = 'none';
 }
