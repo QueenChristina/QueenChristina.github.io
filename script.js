0c5eca5b47ff5b8eed06652f66824d33a3caf901
QueenChristina <qinkristina@gmail.com>
2021-12-23 23:22:58 -0800

    Formatting for phone, next and previous image moves with overlay

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var focusedImg = null;
+
 function show(page) {
     // Switches to the page indicated by pressing menu button
     // Assert page should be an id here
@@ -13,6 +15,7 @@ function show(page) {
 
 // Show closeups of image
 function focusThis(img) {
+    focusedImg = img;
     document.getElementById('focusImg').src = img.src;
     document.getElementById('overlayImg').style.display = 'initial';
 }
@@ -20,3 +23,37 @@ function focusThis(img) {
 function hideOverlay() {
     document.getElementById('overlayImg').style.display = 'none';
 }
+
+function nextImg() {
+    if (focusedImg.nextElementSibling && focusedImg.nextElementSibling.nodeName == 'IMG') {
+        document.getElementById('focusImg').src = focusedImg.nextElementSibling.src;
+        focusedImg = focusedImg.nextElementSibling;
+    } else if (focusedImg.parentElement.id == 'col1') {
+        focusedImg = document.getElementById('col2').firstElementChild;
+        document.getElementById('focusImg').src = focusedImg.src;
+    } else if (focusedImg.parentElement.id == 'col2') {
+        focusedImg = document.getElementById('col1').firstElementChild;
+        document.getElementById('focusImg').src = focusedImg.src;
+    }
+}
+
+function prevImg() {
+    if (focusedImg.previousElementSibling && focusedImg.previousElementSibling.nodeName == 'IMG') {
+        document.getElementById('focusImg').src = focusedImg.previousElementSibling.src;
+        focusedImg = focusedImg.previousElementSibling;
+    } else if (focusedImg.parentElement.id == 'col1') {
+        focusedImg = document.getElementById('col2').lastElementChild;
+        document.getElementById('focusImg').src = focusedImg.src;
+    } else if (focusedImg.parentElement.id == 'col2') {
+        focusedImg = document.getElementById('col1').lastElementChild;
+        document.getElementById('focusImg').src = focusedImg.src;
+    }
+}
+
+// Prevent clicking under to overlay
+// document.addEventListener("load", function(){
+// document.getElementById('focusButton').addEventListener('click', e => {
+//     console.log('afs')
+//     e.stopPropagation();
+// })
+// });
